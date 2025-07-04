85cdfbd9cc21b7806afe9bf99e181342d029ee70
geekyraghav13 <raghavsharma8284@gmail.com>
2025-07-04 14:47:46 +0530

    Difference between val and var

--- app/src/main/java/com/example/kotlinbasics/kotlinbasics.kt
+++ app/src/main/java/com/example/kotlinbasics/kotlinbasics.kt
@@ -2,5 +2,12 @@ package com.example.kotlinbasics
 
 fun main(){
 
-    println("Hello World");
+    println("Hello World")
+    val age = 16
+    println(age) // Val is immmutable and cant be changed
+
+    var myage = 35
+    println(myage)
+    myage =36           // this var we can change and
+    println(myage)
 }
