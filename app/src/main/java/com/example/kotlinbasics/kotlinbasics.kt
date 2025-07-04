253a2a5b9e8012cd2da0ce69485e4ef76baa74f5
geekyraghav13 <raghavsharma8284@gmail.com>
2025-07-04 16:11:28 +0530

    If-Else Statements Practiced

--- app/src/main/java/com/example/kotlinbasics/kotlinbasics.kt
+++ app/src/main/java/com/example/kotlinbasics/kotlinbasics.kt
@@ -2,12 +2,11 @@ package com.example.kotlinbasics
 
 fun main(){
 
-    println("Hello World")
-    val age = 16
-    println(age) // Val is immmutable and cant be changed
-
-    var myage = 35
-    println(myage)
-    myage =36           // this var we can change and
-    println(myage)
+    var age = 35
+    if(age>=18){
+        println("You Can Enter To The Club")
+    }
+    else{
+        println("You Are Too Young For This")
+    }
 }
